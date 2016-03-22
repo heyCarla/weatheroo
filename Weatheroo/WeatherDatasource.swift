0d279a6163d44e4b675487598f1914c037608a00
Carla Alexander <cg.alexander@gmail.com>
2016-03-22 15:19:08 +1100

    updating datasource requests with completion handlers

--- Weatheroo/WeatherDatasource.swift
+++ Weatheroo/WeatherDatasource.swift
@@ -13,12 +13,20 @@ enum WeatherDatasourceError: ErrorType {
     // TODO: add NSError failure types and implement
 }
 
+enum ResultType {
+    case Success(result: String)
+    case Failure(result: String)
+}
+
+typealias WeatherDataCompletion = (result: ResultType) -> Void
+typealias WeatherJSONParsingCompletion  = (currentData: [String:AnyObject]) -> Void
+
 struct WeatherDatasource {
     
+
     // TODO: make/refresh request every 10 minutes
     
-    func makeNetworkRequestWithData() {
-    //makeNetworkRequestWithData(requestData: [String: String])
+    mutating func makeWeatherDataRequest(location: String, completion: WeatherJSONParsingCompletion) { //-> [String:AnyObject]? {
         
         let url     = "http://api.worldweatheronline.com/free/v2/weather.ashx"
         let apiKey  = "5ed0d8b647f24d81a9f105723161603"
@@ -28,21 +36,18 @@ struct WeatherDatasource {
         // q = location
         // num_of_days = number of days
         // key = api key
-        
-        let location = "Sydney"
 
         let endpoint = "\(url)" + "?key=\(apiKey)" + "&q=\(location)" + "&num_of_days=1" + "&format=json"
         //print(endpoint)
         
         ///////////////////////////////////////////////////////////////////////////////////
         
-
         // set up the request
-        let request         = NSMutableURLRequest(URL: NSURL(string: endpoint)!) //TODO: remove (!) when unwrapped
+        let request         = NSMutableURLRequest(URL: NSURL(string: endpoint)!)
         request.HTTPMethod  = "GET"
         
         let session     = NSURLSession(configuration: .defaultSessionConfiguration())
-        let dataTask    = session.dataTaskWithRequest(request)  { (data, response, error) -> Void in
+        let dataTask    = session.dataTaskWithRequest(request) { (data, response, error) -> Void in
             
             guard let data = data where error == nil else {
                 
@@ -54,20 +59,30 @@ struct WeatherDatasource {
             
             let json: [String:AnyObject]
             do {
-                json = try NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers) as! [String : AnyObject]
+                json = try NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers) as! [String: AnyObject]
+                
+                let currentWeatherData = self.currentWeatherConditionsFromJSON(json)
+                completion(currentData: currentWeatherData)
+                //print(currentWeatherData)
+                
             } catch let exception {
                 fatalError("didn't work")
                 
                 // TODO: handle error (invalid json?)
             }
-
             
             // TODO: cache the response
-
-            
-            print(json)
         }
         
         dataTask.resume()
+    }
+    
+    
+    // MARK: JSON Parsing Method(s)
+    
+    func currentWeatherConditionsFromJSON(json: [String:AnyObject]) -> [String:AnyObject] {
+        
+        let currentConditions = json["data"]!["current_condition"]!![0] as! [String:AnyObject]
+        return currentConditions
     }
 }
